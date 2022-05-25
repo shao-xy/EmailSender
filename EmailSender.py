ed31f5c7e2d1479137c847eec2a2099c448fa7ab
Daniel <sxy799@mail.ustc.edu.cn>
2022-05-25 11:26:38 +0800

    Fix sender/receiver name empty bug.

--- EmailSender.py
+++ EmailSender.py
@@ -14,7 +14,23 @@ class EmailBuilder:
   def to_email(self, send_addr: str, recv_addr = '') -> str:
     email = MIMEText(self._content, 'html', 'utf-8')
     email['From'] = Header(self._from_alias and self._from_alias or send_addr, 'utf-8')
+    email['From'].append('<'+send_addr+'>', charset='ascii')
     email['To'] = Header(self._to_alias and self._to_alias or recv_addr, 'utf-8')
+    if recv_addr:
+      email['To'].append('<'+recv_addr+'>', charset='ascii')
+    email['Subject'] = Header(self._subject, 'utf-8')
+    return email.as_string()
+
+  def to_email_batch(self, send_addr: str, recv_addrs: 'list[str]'):
+    email = MIMEText(self._content, 'html', 'utf-8')
+    email['From'] = Header(self._from_alias and self._from_alias or send_addr, 'utf-8')
+    email['From'].append('<'+send_addr+'>', charset='ascii')
+    #email['To'] = Header(self._to_alias and self._to_alias or '', 'utf-8')
+    email['To'] = Header()
+
+    for recv_addr in recv_addrs:
+      email['To'].append(recv_addr, charset='utf-8')
+      email['To'].append('<'+recv_addr+'>', charset='ascii')
     email['Subject'] = Header(self._subject, 'utf-8')
     return email.as_string()
 
@@ -31,23 +47,32 @@ class EmailSender:
     else:
       self._domain = domain
     self._ssl_port = ssl_port
+    self._smtpObj = None
 
   # We don't handle smtplib.SMTPException
   def send(self, eb: EmailBuilder, recv_addrs: 'list[str]'):
     if len(recv_addrs) == 0: return False
 
-    smtpObj = smtplib.SMTP_SSL(self._domain, self._ssl_port)
-    smtpObj.login(self._send_addr, self._passwd)
+    if not self._smtpObj:
+      self._smtpObj = smtplib.SMTP_SSL(self._domain, self._ssl_port)
+      self._smtpObj.login(self._send_addr, self._passwd)
+
     if eb._to_alias:
-      smtpObj.sendmail(self._send_addr, recv_addrs, eb.to_email(self._send_addr, ''))
+      self._smtpObj.sendmail(self._send_addr, recv_addrs, eb.to_email_batch(self._send_addr, recv_addrs))
     else:
       for recv_addr in recv_addrs:
-        smtpObj.sendmail(self._send_addr, [recv_addr], eb.to_email(self._send_addr, recv_addr))
+        self._smtpObj.sendmail(self._send_addr, [recv_addr], eb.to_email(self._send_addr, recv_addr))
+
+    return True
 
 TEST_SENDER = ''
 TEST_PASSWD = ''
-TEST_RECVERS = ['']
+TEST_RECVERS = []
 
 if __name__ == '__main__':
+  #eb = EmailBuilder('Test', 'This is the test body.', 'TrickSender', 'TrickReceiver')
+  #print(eb.to_email(TEST_SENDER, TEST_RECVERS[0]))
+  #print(eb.to_email_batch(TEST_SENDER, TEST_RECVERS))
+
   es = EmailSender(TEST_SENDER, TEST_PASSWD)
   es.send(EmailBuilder('Test', 'This is the test body.', 'TrickSender', 'TrickReceiver'), TEST_RECVERS)
